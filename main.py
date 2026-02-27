8af20d72c092f023cd9d184f48afa9f603ffb309
rozhnovmaxim <rozhnov.maxim@knu.ua>
2026-02-27 15:32:05 +0200

    додано виклик функції НСД

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 from utils import is_prime
 from utils import fact
 from utils import fibonacci
+from utlis import get_gcd
 print (fact(4))
 
 print(f'Is 17 prime? {is_prime(17)}')
 print({fibonacci(10)})
+print(get_gcd(48, 18))
