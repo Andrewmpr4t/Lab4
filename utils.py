05e101a8fb7fb1c11f1a20f85bad0bd31ae9ffce
eugene <evgen.svatnenko@gmail.com>
2026-02-27 15:19:07 +0200

    Додав функцію фібоначі

--- utils.py
+++ utils.py
@@ -9,3 +9,10 @@ def is_prime(n):
     for i in range(2, int(n**0.5) + 1):
         if n % i == 0: return False
     return True
+def fibonacci(n):
+    if n <= 0: return 0
+    elif n == 1: return 1
+    a, b = 0, 1
+    for _ in range(2, n + 1):
+        a, b = b, a + b
+    return b
